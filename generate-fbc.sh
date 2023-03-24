c1ed299a40a97caf0e562a6af48eb764f40a2261
stirabos <stirabos@redhat.com>
2023-03-24 15:11:49 +0100

    Second test for CNV 4.11 from production
    Signed-off-by: Simone Tiraboschi <stirabos@redhat.com>

--- generate-fbc.sh
+++ generate-fbc.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
+OCP_VERSION=4.11
 
 echo "Current OPM version is: $(opm version)"
-echo "Requires OPM v1.26.3+"
+echo "Requires OPM v1.26.4+"
 
 if [ -z "$1" ]; then
   echo "usage: <script> <step #>"
   exit 1
 fi
 
-opm alpha render-veneer basic components/v4.13-${1}.yaml > v4.13/catalog/kubevirt-hyperconverged/catalog.yaml
+mkdir -p v${OCP_VERSION}/catalog/kubevirt-hyperconverged/
+
+opm alpha render-template basic components/v${OCP_VERSION}-${1}.yaml > v${OCP_VERSION}/catalog/kubevirt-hyperconverged/catalog.yaml
